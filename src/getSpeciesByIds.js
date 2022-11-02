b24ae93c82eaf2cf587b3415bb3de3134c2ac523
iriscacais <mandeparairis@gmail.com>
2022-11-01 21:55:30 -0300

    requisito 2

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,11 +1,12 @@
-const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getSpeciesByIds(...ids) {
-  return species.filter((specie) => {
-    if (ids.find((id) => id === specie.id)) {
-      return specie;
-    }
-  });
+  // return data.species.filter((specie) => {
+  //   if (ids.find((id) => id === specie.id)) {
+  //     return specie;
+  //   }
+  // });
 }
+// console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce'));
+
 module.exports = getSpeciesByIds;
