eb6214ff3d1306cc13484ac9435c2a95fddd9335
iriscacais <mandeparairis@gmail.com>
2022-11-03 19:20:17 -0300

    requisito 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,22 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+  return employees.some((employee) => employee.managers.includes(id));
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  const relatedEmployees = [];
+  if (isManager(managerId) === false) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  } if (isManager(managerId) === true) {
+    employees.forEach((employee) => {
+      if (employee.managers.includes(managerId)) {
+        const fullName = `${employee.firstName} ${employee.lastName}`;
+        relatedEmployees.push(fullName);
+      }
+    });
+    return relatedEmployees;
+  }
 }
-
 module.exports = { isManager, getRelatedEmployees };
