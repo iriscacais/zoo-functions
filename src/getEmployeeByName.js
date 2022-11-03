6854929104532168234bbe0a829bcea35a9cb888
iriscacais <mandeparairis@gmail.com>
2022-11-03 13:52:39 -0300

    requisito 1 - corrige lint

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,14 @@
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  // if(!employeeName){
+  //   return {};
+  // }
+  // return employees.find((employee) => {
+  //   if (employeeName === employee.firstName || employee.lastName){
+  //   return employee
+  //   }
+  // });
 }
-
+console.log(getEmployeeByName());
 module.exports = getEmployeeByName;
--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,12 +1,18 @@
 const data = require('../data/zoo_data');
 
 function getSpeciesByIds(...ids) {
-  // return data.species.filter((specie) => {
-  //   if (ids.find((id) => id === specie.id)) {
-  //     return specie;
-  //   }
-  // });
+  if (!ids) {
+    return {};
+  }
+  return data.species.filter((specie) => {
+    const specieByiD = [];
+    if (ids.find((id) => id === specie.id)) {
+      specieByiD.push(specie);
+      return specieByiD;
+    }
+    return specieByiD;
+  });
 }
-// console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce'));
+console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce'));
 
 module.exports = getSpeciesByIds;
