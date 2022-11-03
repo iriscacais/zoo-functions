050e06208f4b758ea840ff347c9d2c342248a654
iriscacais <mandeparairis@gmail.com>
2022-11-03 16:35:13 -0300

    requisito 3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,11 +1,12 @@
-// const { employees } = require('../data/zoo_data');
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
-  // if (!employeeName) {
-  //   return {};
-  // }
-  // return employees.find((employee) => employeeName.includes(employee.firstName) || employeeName.includes(employee.lastName));
+  if (!employeeName) {
+    return {};
+  }
+  return employees.find((employee) => employeeName.includes(employee.firstName) || employeeName
+    .includes(employee.lastName));
 }
 console.log(getEmployeeByName('Bethea'));
 module.exports = getEmployeeByName;
