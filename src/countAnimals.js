d152d9a33486eda8f0fc3a0afaff3d1d3d7a3b33
iriscacais <mandeparairis@gmail.com>
2022-11-04 13:24:24 -0300

    requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,23 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
-}
+  if (!animal) {
+    return species.reduce((acc, cur) => {
+      acc[cur.name] = cur.residents.length;
+      return acc;
+    }, {});
+  }
 
+  const especie = species.find((specie) => specie.name === animal.specie);
+  if (animal.sex !== undefined) {
+    const animalSex = especie.residents.filter((element) => element.sex === animal.sex);
+    return animalSex.length;
+  }
+  return especie.residents.length;
+}
+console.log(countAnimals({ specie: 'penguins' }));
+console.log(countAnimals());
+console.log(countAnimals({ specie: 'giraffes', sex: 'female' }));
+// console.log(countAnimals({ specie: 'giraffes', sex: 'female' }))
 module.exports = countAnimals;
--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -1,3 +1,9 @@
 const handlerElephants = require('../src/handlerElephants');
 
-describe('Testes da função HandlerElephants', () => {});
+describe('Testes da função HandlerElephants', () => {
+  test('Espera que a HandlerElephants exista.', () => {
+    expect(typeof handlerElephants).toBe('function');
+  });
+
+  test('Espera que a averageAge retorne a media de idade dos residents elephants', () => {});
+});
