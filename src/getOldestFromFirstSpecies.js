af22d9d41c6932e476f8ab3ec7e3fbb874650471
iriscacais <mandeparairis@gmail.com>
2022-11-05 20:15:48 -0300

    requisito 10

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,12 @@
+const { employees, species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const colaborador = employees.find((employee) => employee.id === id);
+  const animalID = colaborador.responsibleFor[0];
+  const animal = species.find((specie) => specie.id === animalID).residents
+    .reduce((acc, cur) => (acc.age > cur.age ? acc : cur));
+  return Object.values(animal);
 }
 
 module.exports = getOldestFromFirstSpecies;
