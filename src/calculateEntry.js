77aa2e84ef4dccbac32087778b9959a757cd84e7
iriscacais <mandeparairis@gmail.com>
2022-11-04 21:41:10 -0300

    requisito 8

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,22 @@
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui g
+  const child = entrants.filter((entrant) => entrant.age < 18);
+  const adult = entrants.filter((entrant) => entrant.age >= 18 && entrant.age < 50);
+  const senior = entrants.filter((entrant) => entrant.age >= 50);
+  const sumEntrants = { child: child.length, adult: adult.length, senior: senior.length };
+  return sumEntrants;
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (!entrants || Object.keys(entrants).length === 0) {
+    return 0;
+  }
+  const countOfPeople = countEntrants(entrants);
+  const { child, adult, senior } = countOfPeople;
+  const priceObject = data.prices;
+  const totalPrice = (child * priceObject.child) + (adult * priceObject.adult)
+  + (senior * priceObject.senior);
+  return totalPrice;
 }
-
 module.exports = { calculateEntry, countEntrants };
