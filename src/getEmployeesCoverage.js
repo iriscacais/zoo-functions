68d4998f16799e885c1690cf04b45814ac7b37ab
iriscacais <mandeparairis@gmail.com>
2022-11-06 16:21:27 -0300

    requisito 12

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,24 @@
 const data = require('../data/zoo_data');
+const { employees } = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
+function getEmployeesCoverage(employee) {
+  // if (employees.firstName === employee.value || employees.lastName === employee.value || employees.id === employee.value) {
+
+  // }
+
 }
+// console.log(getEmployeesCoverage({ name: 'Sharonda' }));
 
+// function getEmployees(name) {
+
+// }
+function getSpecies(identification) {
+  const getEmployees = employees.filter((employee) => employee
+    .lastName === identification || employee
+    .firstName === identification || employee.id === identification);
+
+  const getSpeciesResponsable = getEmployees.map((getEmployee) => getEmployee.responsibleFor);
+  return getSpeciesResponsable;
+}
+console.log(getSpecies('Emery'));
 module.exports = getEmployeesCoverage;
--- test/getOpeningHours.test.js
+++ test/getOpeningHours.test.js
@@ -1,3 +1,55 @@
 const getOpeningHours = require('../src/getOpeningHours');
+const { hours } = require('../data/zoo_data');
+const { dayError } = require('../src/getOpeningHours');
 
-describe('Testes da função getOpeningHours', () => {});
+describe('Testes da função getOpeningHours', () => {
+  test('Expect getOpeningHours to be a function.', () => {
+    expect(typeof getOpeningHours).toBe('function');
+  });
+
+  test('to throw a message the zoo is closed if the day is monday', () => {
+    expect(getOpeningHours('Monday', '09:00-AM')).toBe('The zoo is closed');
+  });
+
+  test('expect function getOpeningHours return object Hours if dont receive any parameters', () => {
+    expect(getOpeningHours()).toBe(hours);
+  });
+
+  test('to throw a message the zoo is open if is a day different from monday', () => {
+    expect(getOpeningHours('Tuesday', '09:00-AM')).toBe('The zoo is open');
+  });
+
+  test('to throw an error message if abreviation is different from AM /PM', () => {
+    const actual = () => (getOpeningHours('Tuesday', '09:00-AA'));
+    const expected = 'The abbreviation must be \'AM\' or \'PM\'';
+    expect(actual).toThrow(expected);
+  });
+
+  test('to throw an erros message if number is bigger than 59 mintes', () => {
+    const actual = () => (getOpeningHours('Tuesday', '09:60-AM'));
+    const expected = 'The minutes must be between 0 and 59';
+    expect(actual).toThrow(expected);
+  });
+
+  test('to throw an error message if number is bigger than 12 hours', () => {
+    const actual = () => (getOpeningHours('Tuesday', '13:00-AM'));
+    const expected = 'The hour must be between 0 and 12';
+    expect(actual).toThrow(expected);
+  });
+
+  test('to throw an error message if days are diferent from weekDays', () => {
+    const actual = () => (getOpeningHours('oi', '13:00-AM'));
+    const expected = dayError;
+    expect(actual).toThrow(expected);
+  });
+
+  test('to throw an error message if minutes are different from numbers', () => {
+    const actual = () => (getOpeningHours('Tuesday', '13:a0-AM'));
+    const expected = 'The minutes should represent a number';
+    expect(actual).toThrow(expected);
+  });
+
+  test('to throw a message the zoo is closed if the day is Wednesday e 09:00-PM', () => {
+    expect(getOpeningHours('Wednesday', '09:00-PM')).toBe('The zoo is closed');
+  });
+});
