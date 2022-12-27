2ee6e85c9d6e9596000fb41eb90a97c565839481
Iris Cacais <112999212+iriscacais@users.noreply.github.com>
2022-12-27 10:06:55 -0300

    Update getAnimalsOlderThan.js

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,4 +1,5 @@
-const data = require('../data/zoo_data');
+// Deve receber uma epécie ou uma idade como parâmetro e retornar se todos os animais dessa espécie possuem essa idade ou são mais velhos. Retornar um valor booleano.
+// o que retorna bolleano é some ou every. No caso entao devemos usar every.const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
   const animals = data.species.find((specie) => specie.name === animal);
@@ -6,5 +7,3 @@ function getAnimalsOlderThan(animal, age) {
 }
 console.log(getAnimalsOlderThan('lions', 7));
 module.exports = getAnimalsOlderThan;
-// então retornar se todos os animais dessa espécie possuem essa idade ou são mais velhos.// Retorne um valor booleano.
-// o que retorna bolleano é  some ou every. No caso entao devemos usar every.
