c4218dde69d4bed3955712b7f345740dacc2b592
iriscacais <mandeparairis@gmail.com>
2022-12-26 22:51:21 -0300

    projeto finalizado

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,24 +1,38 @@
 const data = require('../data/zoo_data');
-const { employees } = require('../data/zoo_data');
 
-function getEmployeesCoverage(employee) {
-  // if (employees.firstName === employee.value || employees.lastName === employee.value || employees.id === employee.value) {
+function getEmployeesCoverage(objeto) {
+  // const getEmployee = employees.find((employee) => {
+  //   objeto === employee.firstName || employee.lastName || employee.id
 
-  // }
+  // })
 
+//   const employeeInfo = employees.map((employee) => {
+//     return {
+//       id: employee.id,
+//       fullName: `${employee.firstName} ${employee.lastName}`,
+//       species: employee.responsibleFor,
+//       locations: species.filter((specie) => employee.responsibleFor.includes(specie.id)),
+//     }
+//   })
+// return employeeInfo
 }
 // console.log(getEmployeesCoverage({ name: 'Sharonda' }));
 
 // function getEmployees(name) {
 
 // }
-function getSpecies(identification) {
-  const getEmployees = employees.filter((employee) => employee
-    .lastName === identification || employee
-    .firstName === identification || employee.id === identification);
+// function getSpecies(objeto) {
+//   const getEmployees = employees.filter((employee) => employee
+//     .lastName === Object.values(objeto) || employee
+//     .firstName === Object.values(objeto) || employee.id === Object.values(objeto) );
+//   const getSpeciesResponsable = getEmployees.map((getEmployee) => getEmployee.responsibleFor);
 
-  const getSpeciesResponsable = getEmployees.map((getEmployee) => getEmployee.responsibleFor);
-  return getSpeciesResponsable;
-}
-console.log(getSpecies('Emery'));
+//   return getSpeciesResponsable;
+// }
+
+//   function getLocation(location) {
+
+//   }
+
+// console.log(getSpecies({ name: 'Sharonda' }));
 module.exports = getEmployeesCoverage;
